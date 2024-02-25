8d0d5852327131e023d219bf1d6b5a017c0a99c8
Pablo <st.spasam@gmail.com>
2024-02-25 14:52:55 +0200

    Add formik and yup dependencies, update component import paths, and remove unused code

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import "./App.css";
+import ContactForm from "../ContactForm/ContactForm";
+import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
+
+function App() {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
